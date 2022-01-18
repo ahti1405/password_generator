67ff143dc956706c40075ca9d4b645d4e3e20ec2
ahti1405 <aktan85@mail.ru>
2022-01-18 17:28:16 +0600

    third commit

--- pass_generator/settings.py
+++ pass_generator/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-cxa78ueowb_q4r63-7t!7oi1y66f%iqkdh#x*)pjk*%7f^^ch^'
+SECRET_KEY = 'django-insecure-cxa78ueowb_q4r63-7t!7oi1y66f%iqkdh#x*)pjk*%7f^^ch^ahti'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
